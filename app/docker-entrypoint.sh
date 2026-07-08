fa0fe129daeb165dfabb51b95e684ed3a8fa83ef
Sasha Fortel <166897058+sashathelambo@users.noreply.github.com>
2026-07-07 21:26:47 -0500

    Fix nginx upstream resolution: use resolver directive for runtime DNS, default to api.zeabur.internal

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-envsubst '${API_UPSTREAM}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+RESOLVER=$(awk '/^nameserver/{print $2; exit}' /etc/resolv.conf)
+RESOLVER=${RESOLVER:-127.0.0.11}
+export RESOLVER
+export API_UPSTREAM
+envsubst '${API_UPSTREAM} ${RESOLVER}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
